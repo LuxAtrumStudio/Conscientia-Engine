cfacc8093e871be42a929c609209566f32294a00
Arden <ardenisthebest@gmail.com>
2016-06-02 17:02:37 -0700

    Made to match Windows version

--- Conscientia.cpp
+++ Conscientia.cpp
@@ -9,15 +9,21 @@ using namespace std;
 struct window {
   string name;
   WINDOW * pointer;
-  int posX, posY, sizeX, sizeY;
-  int cursorX, cursorY;
-  int colorPointer;
-  bool border;
+  int startX, startY, sizeX, sizeY, endX, endY;
 };
 vector<window> windows;
 int currentWindowPointer = 0;
 bool autoRefresh = false;
-
+/*=====>>>>>-----ADVNACED DATA-----<<<<<=====*/
+/*=====>>>>>-----Global-----<<<<<=====*/
+struct lux code {
+  vector<string> lines;
+}
+/*>>>>>-----FUNCTIONS-----<<<<<*/
+/*>>>>>-----Menu-----<<<<<*/
+int firstPage= 0, firstList = 0, firstItem = 0;
+/*>>>>>-----Loading Bars-----<<<<<*/
+vector<int> loadingBarPointers;
 namespace Conscientia
 {
   /*=====>>>>>-----FUNCTIONS-----<<<<<=====*/
@@ -28,12 +34,15 @@ namespace Conscientia
     window declaration;
     declaration.name = "stdscr";
     declaration.pointer = stdscr;
-    declaration.posX = 0;
-    declaration.posY = 0;
+    declaration.startX = 0;
+    declaration.startY = 0;
     getmaxyx(stdscr, declaration.sizeY, declaration.sizeX);
     declaration.cursorX = 0;
     declaration.cursorY = 0;
+    declaration.endX = sizeX;
+    declaration.endY = sizeY;
     declaration.border = false;
+    declaration.title = false;
     windows.push_back(declaration);
     initilizeColors();
   }
@@ -73,7 +82,7 @@ namespace Conscientia
   /*=====>>>>>-----Run Time-----<<<<<=====*/
   /*>>>>>-----WINDOW-----<<<<<*/
   /*>>>>>-----Initilization-----<<<<<*/
-  void createWindow(string name, int posX, int posY, int sizeX, int sizeY, bool border){
+  void createWindow(string name, int posX, int posY, int sizeX, int sizeY, bool border, bool title){
     WINDOW *local;
     local = newwin(sizeY, sizeX, posY, posX);
     keypad(local, TRUE);
@@ -83,16 +92,43 @@ namespace Conscientia
     window declaration;
     declaration.name = name;
     declaration.pointer = local;
-    declaration.posX = posX;
-    declaration.posY = posY;
+    declaration.startX = posX;
+    declaration.startY = posY;
     declaration.sizeX = sizeX;
     declaration.sizeY = sizeY;
+    declaration.endX = posX + sizeX;
+    declaration.endY = posY + sizeY;
     declaration.cursorX = 0;
     declaration.cursorY = 0;
     declaration.border = border;
+    declaration.title = title;
+    if(border == true){
+      declaration.cursorX++;
+      declaration.cursorY++;
+    }
+    if(title == true && border == false){
+      declaration.cursorY++;
+    }
+    if(title == true){
+      drawTitle(windows.size() - 1);
+    }
     windows.push_back(declaration);
   }
   /*>>>>>-----Management-----<<<<<*/
+  void clearAllWindows(){
+    for(unsigned a = 0; a < windows.size(); a++){
+      clearWindow(a);
+    }
+  }
+  int findWindowPointer(string name){
+      for(unsigned a = 0; a < windows.size(); a++){
+        if(windows[a].name == name){
+          return(a);
+        }
+      }
+      return(0);
+  }
+  /*-----POINTER-----*/
   void setBorder(int pointer, bool setting){
     if(windows[pointer].border != setting){
       windows[pointer].border = setting;
@@ -104,27 +140,50 @@ namespace Conscientia
       }
     }
   }
-  void clearAllWindows(){
-    for(unsigned a = 0; a < windows.size(); a++){
-      clearWindow(a);
-    }
-  }
   void clearWindow(int pointer){
     wclear(windows[pointer].pointer);
   }
-  int findWindowPointer(string name){
-    for(unsigned a = 0; a < windows.size(); a++){
-      if(windows[a].name == name){
-        return(a);
-      }
+  void setWindowTitle(in tpointer, bool setting){
+    if(windows[pointer].title != setting){
+      windows[pointer].title = setting;
+      drawTitle(pointer);
     }
-    return(0);
   }
-  void setWindowColor(int pointer, int text, int background){
-
+  void setCurrentWindow(int pointer){
+    currentWindowPointer = pointer;
   }
-  void clearWindowColor(int pointer){
-
+  /*-----CURRENT-----*/
+  void csetBorder(bool setting){
+    setBorder(currentWindowPointer, setting);
+  }
+  void cclearWindow(){
+    clearWindow(currentWindowPointer);
+  }
+  void csetWindowTitle(bool setting){
+    setWindowTitle(currentWindowPointer, setting);
+  }
+  /*-----FIND-----*/
+  void fsetBorder(string name, bool setting){
+    setBorder(findWindowPointer(name), setting);
+  }
+  void fclearWindow(string name){
+    clearWindow(findWindowPointer(name));
+  }
+  void fsetWindowtitle(string name, bool setting){
+    setWindowTitle(findWindowPointer(name), setting);
+  }
+  void setCurrentWindow(string name){
+    currentWindowPointer = findWindowPointer(name);
+  }
+  /*-----LINUX-----*/
+  void drawTitle(int pointer){
+    int titleSize = windows[pointer].name.size();
+    int windowSize = windows[pointer].sizeX;
+    int posx;
+    windowSize = widnowSize / 2;
+    titleSize = titleSize / 2;
+    posx = windowSize - titleSize;
+    mvwprintw(windows[pointer].pointer, 0, posx, windows[pointer].name);
   }
   /*>>>>>-----Termination-----<<<<<*/
   void terminateAllWindows(){
@@ -133,29 +192,56 @@ namespace Conscientia
     }
   }
   void terminateWindow(int pointer){
+    if(windows[pointer].border == true){
+      setBorder(poitner, false);
+    }
+    if(windows[pointer].title == true){
+      setWindowTitle(pointer, false);
+    }
     clearWindow(pointer);
+    windows.erase(windows.begin() + pointer);
   }
   /*>>>>>-----USER INTERFACE-----<<<<<*/
   /*>>>>>-----Input-----<<<<<*/
   char gchar(){
-
-  }
-  char cchar(){
-
+    char in;
+    in = getch();
+    return(in);
   }
   int gint(){
-
+    int in;
+    in = int(getch());
+    return(in);
   }
   int cint(){
-
+    int in = 0;
+    int rawin = 0;
+    while(rawin != 13){
+      rawin = int(getch());
+      in = (in * 10) + rawin;
+    }
+    return(in);
   }
   string cstr(){
-
+    string in;
+    char inch;
+    int rawing = 0;
+    while(rawing != 13){
+      rawint = int(getch());
+      inch = char(rawint);
+      in = in + inch;
+    }
+    return(in);
   }
-  float gfloat(){
-
+  float cfloat(){
+    string rawstr = cstr();
+    string::size_type sz;
+    float in;
+    in = stof(rawstr, &sz);
+    return(in);
   }
   /*>>>>>-----Output-----<<<<<*/
+  /*-----POINTER-----*/
   void print(int pointer, string str){
     for(int a = 0; a < str.size(); a++){
       if(str[a] == '/'){
@@ -172,42 +258,56 @@ namespace Conscientia
         }
       }
       else if(str[a] != '/'){
-        string character = "" + str[a];
-        mvwprintw(windows[pointer].pointer, windows[pointer].cursorY, windows[pointer].cursorX, character.c_str());
-        windows[pointer].cursorX++;
-        if(windows[pointer].cursorX > windows[pointer].sizeX){
-          windows[pointer].cursorX = 0;
-          if(windows[pointer].border == true){
-            windows[pointer].cursorX++;
-          }
-          windows[pointer].cursorY++;
-          if(windows[pointer].cursorY > windows[pointer].sizeY){
-            windows[pointer].cursorY = 0;
-            if(windows[pointer].border == true){
-              windows[pointer].cursorY++;
+        if(windows[pointer].border == false){
+          if(windows[pointer].cursorX >= windows[pointer].sizeX){
+            windows[pointer].cursorX = 0;
+            windows[pointer].cursorY++;
+            if(windows[pointer].cursorY >= windows[pointer].sizeY){
+              windows[pointer].cursorY = 0;
+              if(windows[pointer].title == true){
+                windows[pointer].cursorY++;
+              }
+              clearWindow(pointer);
             }
-            clearWindow(pointer);
           }
         }
+        if(wndows[pointer].border == true){
+          if(windows[pointer].cursorX >= windows[pointer].sizeX - 1){
+            windows[pointer].cursorX = 1;
+            windows[pointer].cursorY++;
+            if(windows[pointer].cursorY >= windows[pointer].sizeY - 1){
+              windows[pointer].cursorY = 1;
+              clearWindow(pointer);
+            }
+          }
+        }
+        string character;
+        stringstream sstream;
+        sstream << str[a];
+        sstream >> character;
+        mvwprintw(pointer, windows[pointer].cursorY, windows[pointer].cursorX, character);
+        windows[pointer].cursorX++;
       }
     }
     if(autoRefresh == true){
-      updateWindow(pointer);
+      update(pointer);
     }
-  }
-  void cprint(string str){
-    print(currentWindowPointer, str);
-  }
-  void fprint(string name, string str){
-    print(findWindowPointer(name), str);
   }
   void mprint(int pointer, int x, int y, string str){
     windows[pointer].cursorX = x;
     windows[pointer].cursorY = y;
     print(pointer, str);
   }
+  /*-----CURRENT-----*/
+  void cprint(string str){
+    print(currentWindowPointer, str);
+  }
   void cmprint(int x, int y, string str){
     mprint(currentWindowPointer, x, y, str);
+  }
+  /*-----FIND-----*/
+  void fprint(string name, string str){
+    print(findWindowPointer(name), str);
   }
   void fmprint(string name, int x, int y, string str){
     int pointer = findWindowPointer(name);
@@ -216,16 +316,9 @@ namespace Conscientia
   /*>>>>>-----SYSTEM-----<<<<<*/
   /*>>>>>-----Update-----<<<<<*/
   void update(){
-    updateAllWindows();
-  }
-  /*-----NCURSES-----*/
-  void updateAllWindows(){
     for(unsigned a = 0; a < windows.size(); a++){
-      updateWindow(a);
+      wrefresh(windows[a].pointer);
     }
-  }
-  void updateWindow(int pointer){
-    wrefresh(windows[pointer].pointer);
   }
   /*=====>>>>>-----Termination-----<<<<<=====*/
   void terminateConscientia(){
--- Conscientia.h
+++ Conscientia.h
@@ -1,57 +1,89 @@
 #include <iostream>
+#include <string>
 #include <vector>
-#include <ncurses.h>
+#include <fstream>
+#include <ctime>
 using namespace std;
-
-namespace Conscientia
-{
-  /*=====>>>>>-----FUNCTIONS-----<<<<<=====*/
-  /*=====>>>>>-----Initilization-----<<<<<=====*/
-  void initilizeConscientia();
-  void advancedInit(bool cursor, bool echo, bool raw);
-  /*>>>>>-----SETTINGS-----<<<<<*/
-  void setAutoRefresh(bool setting);
-  /*>>>>>-----NCURSES INITILIZATION-----<<<<<*/
-  void initilizeColors();
-  void setCursor(bool setting);
-  void setEcho(bool setting);
-  void setRaw(bool setting);
-  /*=====>>>>>-----Run Time-----<<<<<=====*/
-  /*>>>>>-----WINDOW-----<<<<<*/
-  /*>>>>>-----Initilization-----<<<<<*/
-  void createWindow(string name, int posX, int posY, int sizeX, int sizeY, bool border);
-  /*>>>>>-----Management-----<<<<<*/
-  void drawAllBorders();
-  void drawBorder(int pointer);
-  void clearAllWindows();
-  void clearWindow(int pointer);
-  int findWindowPointer(string name);
-  void setWindowColor(int pointer, int text, int background);
-  void clearWindowColor(int pointer);
-  /*>>>>>-----Termination-----<<<<<*/
-  void terminateAllWindows();
-  void terminateWindow(int pointer);
-  /*>>>>>-----USER INTERFACE-----<<<<<*/
-  /*>>>>>-----Input-----<<<<<*/
-  char gchar();
-  char cchar();
-  int gint();
-  int cint();
-  string cstr();
-  float gfloat();
-  /*>>>>>-----Output-----<<<<<*/
-  void print(int pointer, string str);
-  void cprint(string str);
-  void fprint(string name, string str);
-  void mprint(int pointer, int x, int y, string str);
-  void cmprint(int x, int y, string str);
-  void fmprint(string name, int x, int y, string str);
-  /*>>>>>-----SYSTEM-----<<<<<*/
-  /*>>>>>-----Update-----<<<<<*/
-  void update();
-  /*-----NCURSES-----*/
-  void updateAllWindows();
-  void updateWindow(int pointer);
-  /*=====>>>>>-----Termination-----<<<<<=====*/
-  void terminateConscientia();
+struct menuList {
+	string name;
+	vector<string> items;
 };
+struct menuPage {
+	string name;
+	vector<menuList> lists;
+};
+struct menuHierarchy {
+	string name;
+	vector<menuPage> pages;
+};
+namespace CONSCIENTIA {
+	/*=====>>>>>-----CORE FUNCTIONS-----<<<<<=====*/
+	/*=====>>>>>-----Initilization-----<<<<<=====*/
+	void initializeConscientia();
+	void advancedInit(bool cursor, bool echo, bool raw);
+	/*>>>>>-----SETTINGS-----<<<<<*/
+	void setAutoRefresh(bool setting);
+	/*=====>>>>>-----Run Time-----<<<<<=====*/
+	/*>>>>>-----WINDOW-----<<<<<*/
+	/*>>>>>-----Initilization-----<<<<<*/
+	void createWindow(string name, int posX, int posY, int sizeX, int sizeY, bool border, bool title);
+	/*>>>>>-----Management-----<<<<<*/
+	void clearAllWindows();
+	int findWindowPointer(string name);
+	/*-----POINTER-----*/
+	void setBorder(int pointer, bool setting);
+	void clearWindow(int pointer);
+	void setWindowTitle(int pointer, bool setting);
+	void setCurrentWindow(int pointer);
+	/*-----CURRENT-----*/
+	void csetBorder(bool setting);
+	void cclearWindow();
+	void csetWindowTitle(bool setting);
+	/*-----FIND-----*/
+	void fsetBorder(string name, bool setting);
+	void fclearWindow(string name);
+	void fsetWindowTitle(string name, bool setting);
+	void fsetCurrentWindow(string name);
+	/*-----NCURSES-----*/
+	void drawTitle(int pointer);
+	/*>>>>>-----Termination-----<<<<<*/
+	void terminateAllWindows();
+	void terminateWindow(int pointer);
+	/*>>>>>-----USER INTERFACE-----<<<<<*/
+	/*>>>>>-----Input-----<<<<<*/
+	char gchar();
+	int gint();
+	int cint();
+	string cstr();
+	float cfloat();
+	/*>>>>>-----Output-----<<<<<*/
+	/*-----POINTER-----*/
+	void print(int pointer, string str);
+	void mprint(int pointer, int x, int y, string str);
+	/*-----CURRENT-----*/
+	void cprint(string str);
+	void cmprint(int x, int y, string str);
+	/*-----FIND-----*/
+	void fprint(string name, string str);
+	void fmprint(string name, int x, int y, string str);
+	/*>>>>>-----SYSTEM-----<<<<<*/
+	/*>>>>>-----Update-----<<<<<*/
+	void update();
+	/*=====>>>>>-----Termination-----<<<<<=====*/
+	void terminateConscientia();
+	/*=====>>>>>-----ADVANCED FUNCITONS-----<<<<<=====*/
+	/*=====>>>>>-----Output Funcitons-----<<<<<=====*/
+	/*>>>>>-----INTERACTIVE-----<<<<<*/
+	/*>>>>>-----Menu-----<<<<<*/
+	string menu(string menuFileDirectory, int posX, int posY, int sizeX, int sizeY);
+	menuHierarchy loadMenuHierarchy(string menuFileDirectory);
+	void displayMenu(menuHierarchy menu, int currentPage, int currentList, int currentItem);
+	int findTextStart(string str, int space);
+	/*>>>>>-----DISPLAY-----<<<<<*/
+	/*>>>>>-----Loading Bars-----<<<<<*/
+	int initializeLoadingBar(string process);
+	void loadingBar(int index, double percent);
+	void terminateLoadingBar(int index);
+	/*=====>>>>>-----Input Funcitons-----<<<<<=====*/
+	/*=====>>>>>-----System Funcitons-----<<<<<=====*/
+}
